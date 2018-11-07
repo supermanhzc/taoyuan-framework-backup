4a1e617d454dec82111aa99c8c0409d9ec8b6ef6
dongwen <dw96998@163.com>
2018-11-08 00:28:33 +0800

    add aaa file

--- aaa/src/main/java/com/taoyuan/framework/aaa/entity/SysPermission.java
+++ aaa/src/main/java/com/taoyuan/framework/aaa/entity/SysPermission.java
@@ -3,18 +3,22 @@ package com.taoyuan.framework.aaa.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName(value = "ty_permission")
 public class SysPermission {
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
     private String name;
+    @TableField(value = "parent_id")
     private Long parentId;
+    @TableField(exist = false)
     private String parentIds;
     private String permission;
+    @TableField(value = "resource_type")
     private String resourceType;
     private String url;
-    @TableField(value = "available")
-    private Boolean isAvailable;
+    private Boolean enabled;
 }
--- aaa/src/main/java/com/taoyuan/framework/aaa/entity/UserInfo.java
+++ aaa/src/main/java/com/taoyuan/framework/aaa/entity/UserInfo.java
@@ -2,11 +2,13 @@ package com.taoyuan.framework.aaa.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@TableName(value = "ty_user")
 public class UserInfo implements Serializable {
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
