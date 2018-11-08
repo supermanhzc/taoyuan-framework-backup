af821f792994bd68bf8e19ce3438c72a69b1b805
zopnh <huzhengchao01@saicmotor.com>
2018-11-08 11:18:41 +0800

    增加http时间格式处理，同时增加异常参数

--- common/src/main/java/com/taoyuan/framework/common/exception/ValidateException.java
+++ common/src/main/java/com/taoyuan/framework/common/exception/ValidateException.java
@@ -4,4 +4,12 @@ public class ValidateException extends TyException {
     public ValidateException(Integer code, String msg) {
         super(code, msg);
     }
+
+    public ValidateException(Integer code, String msg, Object param) {
+        super(code, msg, param);
+    }
+
+    public ValidateException(Integer code, String msg, Object[] param) {
+        super(code, msg, param);
+    }
 }
