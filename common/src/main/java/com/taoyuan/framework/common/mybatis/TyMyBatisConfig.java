cfc5616daba34c8d43f9d2564c1e8ca0934b3117
supermanhzc <supermanhzc@163.com>
2018-11-11 21:01:27 +0800

    mybatis分页配置增加

---
+++ common/src/main/java/com/taoyuan/framework/common/mybatis/TyMyBatisConfig.java
@@ -0,0 +1,15 @@
+package com.taoyuan.framework.common.mybatis;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TyMyBatisConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+        return page;
+    }
+}
