7732c7e55981ea6b244e69ab18f850fa340a1169
zopnh <huzhengchao01@saicmotor.com>
2018-11-27 16:12:52 +0800

    代码优化

---
+++ aaa/src/main/java/com/taoyuan/framework/oper/IProxyOperService.java
@@ -0,0 +1,7 @@
+package com.taoyuan.framework.oper;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.taoyuan.framework.common.entity.TyProxyOperation;
+
+public interface IProxyOperService extends IService<TyProxyOperation> {
+}
