e3c2abf153e00c81764883464ec38df763034ff6
dongwen <dw96998@163.com>
2018-12-06 22:28:08 +0800

    spring boot版本降级至2.0.x

--- aaa/src/main/java/com/taoyuan/framework/aaa/config/RestAuthenticationFilter.java
+++ aaa/src/main/java/com/taoyuan/framework/aaa/config/RestAuthenticationFilter.java
@@ -1,5 +1,8 @@
 package com.taoyuan.framework.aaa.config;
 
+import com.taoyuan.framework.common.constant.ResultCode;
+import com.taoyuan.framework.common.http.TyResponse;
+import net.sf.json.JSONObject;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 
 import javax.servlet.ServletRequest;
@@ -14,6 +17,15 @@ public class RestAuthenticationFilter extends FormAuthenticationFilter {
         HttpServletResponse httpServletResponse = (HttpServletResponse) response;
         httpServletResponse.setCharacterEncoding("UTF-8");
         httpServletResponse.setContentType("application/json");
+
+        TyResponse tyResponse = new TyResponse();
+        tyResponse.setCode(ResultCode.USER_NOT_LOGGED_IN.getCode().toString());
+        tyResponse.setMsg(ResultCode.USER_NOT_LOGGED_IN.getMsg());
+        JSONObject jsonObject = JSONObject.fromObject(tyResponse);
+        httpServletResponse.getWriter().write(jsonObject.toString());
+        httpServletResponse.getWriter().flush();
+        httpServletResponse.getWriter().close();
+
         return false;
     }
 }
