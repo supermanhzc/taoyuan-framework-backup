1c4d35933b051635ebd2429c2acbf64f24374f14
dongwen <dw96998@163.com>
2018-12-12 23:53:25 +0800

    修正异常情况的操作日志

--- bs/src/main/java/com/taoyuan/framework/bs/aspect/OperLogAspect.java
+++ bs/src/main/java/com/taoyuan/framework/bs/aspect/OperLogAspect.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.taoyuan.framework.bs.entity.operlog.TyOperLog;
 import com.taoyuan.framework.bs.service.operlog.TyOperLogService;
 import com.taoyuan.framework.common.entity.TyUserRolePermission;
+import com.taoyuan.framework.common.exception.TyException;
 import com.taoyuan.framework.common.http.TyResponse;
 import com.taoyuan.framework.common.http.TySession;
 import lombok.extern.slf4j.Slf4j;
@@ -53,6 +54,7 @@ public class OperLogAspect {
         tyOperLog.setRequest(this.getRequestInfo(joinPoint, request));
         this.setControllerAnnotationArgs(joinPoint, tyOperLog);
         tyOperLog.setStartTime(new Date());
+        request.setAttribute("operlog", tyOperLog);
         Object proceed = joinPoint.proceed();
         tyOperLog.setEndTime(new Date());
         TyResponse response = (TyResponse) proceed;
@@ -70,24 +72,20 @@ public class OperLogAspect {
 
     @AfterThrowing(pointcut = "controllerAspect()",throwing="e")
     public void doAfterThrowing(JoinPoint joinPoint, Throwable e) throws Throwable{
-        TyOperLog tyOperLog = new TyOperLog();
-        Object proceed = null ;
+
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         HttpServletRequest request = attributes.getRequest();
-        log.info("Receive a request from host: {}, url: {} {}, querystr: {}, args: {}", request.getRemoteAddr(),
-                request.getMethod(), request.getRequestURL().toString(), JSON.toJSONString(request.getParameterMap(),
-                        SerializerFeature.DisableCircularReferenceDetect, SerializerFeature.WriteMapNullValue),
-                JSON.toJSONString(joinPoint.getArgs()));
 
-        tyOperLog.setIp(request.getRemoteAddr());
-        TyUserRolePermission currentUser = TySession.getCurrentUser();
-        if(null != currentUser){
-            tyOperLog.setUserId(currentUser.getUserId());
-        }
+        TyOperLog tyOperLog = (TyOperLog) request.getAttribute("operlog");
         tyOperLog.setEndTime(new Date());
-        tyOperLog.setRequest(this.getRequestInfo(joinPoint, request));
-        tyOperLog.setRespCode(e.getClass().getName());
-        tyOperLog.setRespMessage(e.getMessage());
+        if(e instanceof TyException){
+            TyException tyException = (TyException) e;
+            tyOperLog.setRespCode(tyException.getCode().toString());
+            tyOperLog.setRespMessage(tyException.getMessage());
+        } else{
+            tyOperLog.setRespCode(e.getClass().getName());
+            tyOperLog.setRespMessage(e.getMessage());
+        }
         log.info("exception happened. " + e.getMessage());
         tyOperLogService.save(tyOperLog);
     }
